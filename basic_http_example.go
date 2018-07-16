e525a3dcb2f7c995b8da0b590aa9f2074c7eda16
root <root@ACESSO04.laboratorio.dimep.com.br>
2018-07-16 19:22:41 +0000

    Fix imports

--- basic_http_example.go
+++ basic_http_example.go
@@ -1,11 +1,17 @@
+package main
+
+import "fmt"
+import "net/http"
+import "log"
+
 func main() {
 	port:= 8080
 
 	http.HandleFunc("/helloworld", helloWorldHandler)
-	log.Printf("Server stating on port %v\n")
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v, port), nil)
+	log.Printf("Server stating on port %v\n", port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", port), nil))
 }
 
-func HelloWorldHandler(w http.ResponseWriter, r *http.Request) {
+func helloWorldHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Hello World\n")
 }
