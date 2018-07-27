e9409e63cc598bdf666c9c855964279ae185c98c
Danillo Martins <danillomartinsdev@gmail.com>
2018-07-27 17:40:17 +0000

    Json and Struct

--- basic_http_example.go
+++ basic_http_example.go
@@ -3,6 +3,7 @@ package main
 import "fmt"
 import "net/http"
 import "log"
+import "encoding/json"
 
 func main() {
 	port:= 8080
@@ -12,6 +13,24 @@ func main() {
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", port), nil))
 }
 
-func helloWorldHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello World\n")
+type helloWorldResponse struct {
+// change the output field to be "messge"
+         Message string `json:"message"`
+// do not output this field
+	Author string `json:"-"`
+// do not output this field if the value is empty
+	Date string `json:",omitempty"`
+// convert output to a string and rename "id"
+	Id int `json:"id, string"`
 }
+
+func helloWorldHandler(w http.ResponseWriter, r *http.Request) {
+	response := helloWorldResponse {Message: "Hello World"}
+	data, err :=  json.Marshal(response)
+	if err != nil {
+	  panic("Ooops")
+	}
+	fmt.Fprint(w, string(data))
+}
+
+
